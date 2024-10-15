3d581defd10901c7a370997f53b7884fdc6770e8
doubletroublepy <doubletroublepy@gmail.com>
2024-10-15 22:27:38 +0200

    .

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -52,6 +52,21 @@ require("lazy").setup({
   },
 })
 
+require("neo-tree").setup({
+  event_handlers = {
+
+    {
+      event = "file_open_requested",
+      handler = function()
+        -- auto close
+        -- vim.cmd("Neotree close")
+        -- OR
+        require("neo-tree.command").execute({ action = "close" })
+      end,
+    },
+  },
+})
+
 return {
   -- add pyright to lspconfig
   {
